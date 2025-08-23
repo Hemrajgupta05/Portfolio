300899bc8c7a9a2911bbd3773b4d7f0fd2764242
Hemraj Gupta <hemrajgupta2003@gmail.com>
2025-08-23 09:07:22 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Hemraj Gupta</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} HG</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
