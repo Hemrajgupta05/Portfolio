2a23a8d864b2002ce6a07db2444eca8a0bd0228a
Hemraj Gupta <hemrajgupta2003@gmail.com>
2025-08-30 14:44:05 +0530

    Update ProjectCards.js
    Resolved merge conflicts and added Live Demo button

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,40 +1,38 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { Card, Button, ButtonGroup } from "react-bootstrap";  // Import ButtonGroup for side-by-side buttons
+import { BsGithub } from "react-icons/bs";  // Correct import for GitHub icon
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} className="card-img" />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+        {/* Button group for GitHub and Live Demo */}
+        <ButtonGroup>
+          <Button variant="primary" href={props.gLink} target="_blank">
+            <BsGithub /> &nbsp; {props.isBlog ? "Blog" : "GitHub"}
           </Button>
-        )}
+          
+          {/* Only show Live Demo button if the liveDemoLink exists and the project is not a Blog */}
+          {props.liveDemoLink && !props.isBlog && (
+            <Button
+              variant="secondary"
+              href={props.liveDemoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}  // Add spacing between buttons
+            >
+              Live Demo
+            </Button>
+          )}
+        </ButtonGroup>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
